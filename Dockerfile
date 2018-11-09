7ee55ee43834e88dc61260cd31b9f8e2068e8867
kondo takeshi <chaspy@users.noreply.github.com>
2018-11-10 02:31:34 +0900

    Hello, heroku

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM golang:1.11.2-stretch
 ENV GOPATH /go
+ENV PORT
 
 RUN go get -u github.com/golang/dep/cmd/dep
 
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	_ "github.com/chaspy/todo/routers"
 	"github.com/astaxie/beego"
+  "os"
+	"strconv"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+    beego.BConfig.Listen.HTTPPort = port
+	}
+
 	beego.Run()
 }
-
